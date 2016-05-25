b91e375327d9aab7c2f689f4c3588b4861a526f1
Abhi <abhi.kala123@gmail.com>
2016-05-25 15:10:17 +1200

    basic server with the main hbs working

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express')
 var hbs = require('express-handlebars')
 var path = require('path')
 
+var data = require('./data')
+
 var app = express()
 
 var PORT = process.env.PORT || 3000
@@ -9,6 +11,13 @@ var PORT = process.env.PORT || 3000
 app.engine('hbs', hbs({
   extname: 'hbs'
 }))
+
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get('/', function (req, res) {
+  res.render('home', data)
+})
 
 app.listen(PORT, function () {
   console.log('Listening on port :', PORT);
